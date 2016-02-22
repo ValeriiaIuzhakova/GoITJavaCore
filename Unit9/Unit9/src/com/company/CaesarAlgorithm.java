bcf83eee3e64b3123a1c968dd4d3010c9e2d1e34
Valeriia Iuzhakova <sv2007g@gmail.com>
2016-02-22 23:57:11 +0200

    Homework for Unit9. Caesar algorithm(Updated)

--- Unit9/Unit9/src/com/company/CaesarAlgorithm.java
+++ Unit9/Unit9/src/com/company/CaesarAlgorithm.java
@@ -4,34 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CaesarAlgorithm {
-
-    private static final int SIZE = 26;
+    
     private static char[] symbols = {'.', ',', ';', ':', '!', '?', '-', ' '};
     private static List<Character> alphabet;
 
     public CaesarAlgorithm() {
         alphabet = new ArrayList<Character>();
 
-        for(int i = 0; i < SIZE; i++) {
-            if(i <= 9) {
-                alphabet.add(Character.forDigit(i, 10));
-            }
-            alphabet.add((char)(65 + i));
-            alphabet.add((char)(97 + i));
+        for (int i = 0; i <= 9; i++) {
+            alphabet.add(Character.forDigit(i, 10));
         }
 
-        for(char c : symbols) {
+        for (char c = 'a'; c <= 'z'; c++) {
             alphabet.add(c);
         }
 
-        //System.out.println(alphabet);
+        for (char c = 'A'; c <= 'Z'; c++) {
+            alphabet.add(c);
+        }
+
+        for (char c : symbols) {
+            alphabet.add(c);
+        }
     }
 
-    public String encode(String phrase) {
+    public String encode (String phrase){
         StringBuilder encrypted = new StringBuilder();
         int alphabetSize = alphabet.size();
 
-        for(int i = 0; i < phrase.length(); i++) {
+        for (int i = 0; i < phrase.length(); i++) {
             char encChar = phrase.charAt(i);
             int index = alphabet.indexOf(encChar);
             index = (index + 3) % alphabetSize;
@@ -41,15 +42,15 @@ public class CaesarAlgorithm {
         return encrypted.toString();
     }
 
-    public String decode(String phrase) {
+    public String decode (String phrase){
         StringBuilder decrypted = new StringBuilder();
         int alphabetSize = alphabet.size();
 
-        for(int i = 0; i < phrase.length(); i++) {
+        for (int i = 0; i < phrase.length(); i++) {
             char decChar = phrase.charAt(i);
             int index = alphabet.indexOf(decChar);
-            index = (index  - 3) % alphabetSize;
-            if(index < 0) {
+            index = (index - 3) % alphabetSize;
+            if (index < 0) {
                 index = index + alphabetSize;
             }
             decrypted.append(alphabet.get(index));
